c9f6af900cca08db31183dae333f0c1643514603
Lidia Vasileva <lydia.r.vasileva@gmail.com>
2017-05-15 11:36:12 +0200

    made into functions, started update

--- tiny-db-test.py
+++ tiny-db-test.py
@@ -1,11 +1,3 @@
-#table- items
-#idItem - nameItem - statusItem - nameUser
-
-#Example:
-#1 - PC - 10-20-1344(return date) - lidi0168
-#1 - PC - INSTOCK - INHOUSE
-
-
 from tinydb import TinyDB, Query
 #set the default table name
 TinyDB.DEFAULT_TABLE = 'itemTable'
@@ -14,19 +6,27 @@ TinyDB.DEFAULT_TABLE = 'itemTable'
 #for a single instance only, passing default_table to the TinyDB constructor
 #TinyDB(storage=SomeStorage, default_table='my-default')
 
-
-#Create the DB as a JSON file
-
-    #do this only if db does not exist
+#Create the DB as a JSON file, open JSON file
 db = TinyDB('//home/lidia/git/sis-project/db-library.json')
 
 #NOTE: To use the in-memory storage, use:
 #from tinydb.storages import MemoryStorage
 #db = TinyDB(storage=MemoryStorage)
 
+#Add new items
+def addItem():
+    #Make this into terminal variables? Or GUI?
+    nameItem = input("Insert Name of the Item: ")
+    statusItem = input("Insert the Status of the Item: ")
+    nameUser = input("Insert the User name: ")
 
-nameItem = input("Insert Name of the Item: ")
-statusItem = input("Insert the Status of the Item: ")
-nameUser = input("Insert the User name: ")
+    #NOTE: TinyDB adds IDs to each entry
+    db.insert_multiple([{'nameItem': nameItem , 'statusItem': statusItem, 'nameUser': nameUser}])
 
-db.insert_multiple([{'nameItem': nameItem , 'statusItem': statusItem, 'nameUser': nameUser}])
+#Update items
+def updateItem():
+    userUpdate = input("What do you want to update?")
+    db.update(fields, query)
+
+addItem()
+updateItem()
