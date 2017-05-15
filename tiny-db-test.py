821853fe171381d3825aa785c997d36c685f74d9
Lidia Vasileva <lydia.r.vasileva@gmail.com>
2017-05-15 08:40:13 +0200

    added tiny-db-test.py and playing around with it

---
+++ tiny-db-test.py
@@ -0,0 +1,19 @@
+#from tinydb import TinyDB, Query
+#db = TinyDB('//home/lidia/git/sis-project/db.json')
+#db.insert({'int': 1, 'char': 'a'})
+#db.insert({'int': 1, 'char': 'b'})
+
+#THIS WORKS ^
+
+#What do we need in the db
+
+#table 1 - items
+#idItem - nameItem - statusItem - userId
+
+#Example:
+#1 - PC - 10-20-1344(return date) - 2
+#1 - PC - INSTOCK - 0
+
+#table 2 - users
+#Example:
+#idUser - nameUser
