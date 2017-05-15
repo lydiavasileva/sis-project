5b031cb9f1b9f40337ce0c894b61f08e16f382b9
Lidia Vasileva <lydia.r.vasileva@gmail.com>
2017-05-15 13:02:29 +0200

    added parsing

--- add_item.py
+++ add_item.py
@@ -1,16 +1,17 @@
 from tinydb import TinyDB, Query
-import argparse
+import sys
 
 if __name__ == "__main__":
+
     TinyDB.DEFAULT_TABLE = 'itemTable'
     db = TinyDB('//home/lidia/git/sis-project/db-library.json')
 
-    def addItem():
+    def add_item():
 
-        nameItem = input("Insert Name of the Item: ")
-        statusItem = input("Insert the Status of the Item: ")
-        nameUser = input("Insert the User name: ")
+        nameItem = sys.argv[1]
+        statusItem = sys.argv[2]
+        nameUser = sys.argv[3]
 
         db.insert_multiple([{'nameItem': nameItem , 'statusItem': statusItem, 'nameUser': nameUser}])
 
-    addItem()
+    add_item()
