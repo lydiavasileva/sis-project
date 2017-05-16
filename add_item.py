7689cf94fe8464f21ee9a8843bd83d1df83de490
Lidia Vasileva <lydia.r.vasileva@gmail.com>
2017-05-16 10:39:39 +0200

    clean up add_item.py

--- add_item.py
+++ add_item.py
@@ -1,40 +1,16 @@
-#from tinydb import TinyDB, Query
-#import sys
 import argparse
 from db import DataBase
 
 def get_cmd_args():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('itemname', help='the name of the item to process')
-    parser.add_argument('status', help='the name of the item to process')
-    parser.add_argument('user', help='the name of the item to process')
+    parser = argparse.ArgumentParser(description = "Get 'itemname', 'status' and 'user' from user input.")
+    parser.add_argument('itemname', help='The name of the item to pro cess.')
+    parser.add_argument('status', help='The status of named item.')
+    parser.add_argument('user', help='The user that is responsible for the item.')
 
     args = parser.parse_args()
     return args
-
-# def init_db( db_filename = '//home/lidia/git/sis-project/db-library.json'):
-#     TinyDB.DEFAULT_TABLE = 'itemTable'
-#     db = TinyDB( db_filename )
-#     return db
-
-# def insert_item( db, item ):
-#     db.insert_multiple([{"nameItem": config.itemname ,"statusItem": config.status, "nameUser": config.user}])
 
 if __name__ == "__main__":
     config = get_cmd_args()
     db = DataBase('//home/lidia/git/sis-project/db-library.json')
     db.add(config.itemname, config.status, config.user)
-
-    # insert_item( db, item )
-#    update_item()
-#    delete_item()
-
-   # def add_item():
-   #
-   #     nameItem = sys.argv[1]
-   #     statusItem = sys.argv[2]
-   #     nameUser = sys.argv[3]
-   #
-   #     db.insert_multiple([{"nameItem": nameItem ,"statusItem": statusItem,"nameUser": nameUser}])
-
-    #add_item()
